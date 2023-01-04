b448b6cab5b4852dfdcc332b9787e9bdc698e71e
Vitaliy Roschin <kot.official@yandex.ru>
2023-01-04 08:24:35 +0300

    исправил ошибки

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,50 +1,49 @@
 const { celebrate, Joi } = require('celebrate');
-const regExpUrl = require('../utils/constants');
 
 module.exports.createCardValidator = celebrate({
-  body: Joi.object.keys({
+  body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
-    link: Joi.string().required().regex(regExpUrl),
+    link: Joi.string().required().regex(/https?:\/\/\S+/),
   }),
 });
 
 module.exports.cardIdValidator = celebrate({
-  params: Joi.object.keys({
+  params: Joi.object().keys({
     cardId: Joi.string().alphanum().length(24),
   }),
 });
 
 module.exports.createUserValidator = celebrate({
-  body: Joi.object.keys({
+  body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
     about: Joi.string().min(2).max(30).required(),
-    avatar: Joi.string().regex(regExpUrl),
+    avatar: Joi.string().regex(/https?:\/\/\S+/),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
   }),
 });
 
 module.exports.updateUserValidator = celebrate({
-  body: Joi.object.keys({
+  body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
     about: Joi.string().min(2).max(30).required(),
   }),
 });
 
-module.exports.updateUserAvatar = celebrate({
-  body: Joi.object.keys({
-    avatar: Joi.string().regex(regExpUrl),
+module.exports.updateAvatarValidator = celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().regex(/https?:\/\/\S+/),
   }),
 });
 
 module.exports.userIdValidator = celebrate({
-  params: Joi.object.keys({
+  params: Joi.object().keys({
     userId: Joi.string().alphanum().length(24),
   }),
 });
 
 module.exports.loginValidator = celebrate({
-  body: Joi.object.keys({
+  body: Joi.object().keys({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
   }),
