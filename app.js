22f46c09b2ca9f77f064cfcc08fc6debde95ee4e
Vitaliy Roschin <kot.official@yandex.ru>
2023-01-15 11:58:40 +0300

    отключил helmet

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const helmet = require('helmet');
 const { errors } = require('celebrate');
 const errorHandler = require('./middlewares/errorHandler');
 const NotFoundError = require('./errors/NotFoundError');
@@ -13,7 +12,6 @@ const { createUserValidator, loginValidator } = require('./middlewares/validatio
 const { PORT = 3000 } = process.env;
 const app = express();
 app.use(bodyParser.json());
-app.use(helmet());
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
